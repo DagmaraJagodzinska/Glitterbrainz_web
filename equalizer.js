8b7a0b44fd120e96e92848dca64eff7f88db9b1a
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-01-03 22:19:42 +0100

    files to swich and add

--- equalizer.js
+++ equalizer.js
@@ -1,20 +1,21 @@
-$(document).ready(function() {
-  /* Every time the window is scrolled ... */
-  $(window).scroll(function() {
-    /* Check the location of each desired element */
-    $(".equilizer").each(function(i) {
-      var bottom_of_object =
-        $(this).offset().top + $(this).outerWidth(); // cause of rotation 90deg
-      var middle_of_object =
-        $(this).offset().top + $(this).outerWidth()/2;
-      var bottom_of_window = $(window).scrollTop() + $(window).height();
-var top_of_window = $(window).scrollTop();
-      /* If the object is completely visible in the window, fade it it */
-      if (bottom_of_window > middle_of_object && top_of_window < middle_of_object) {
-        $(this).addClass("visible");
-      } else {
-        $(this).removeClass("visible");
-      }
+$(document).ready(function () {
+    /* Every time the window is scrolled ... */
+    $(window).scroll(function () {
+        /* Check the location of each desired element */
+        $(".equilizer").each(function (i) {
+            var bottom_of_object =
+                $(this).offset().top + $(this).outerHeight();
+            var top_of_object = $(this).offset().top;
+            var middle_of_object =
+                $(this).offset().top + $(this).outerHeight() / 2;
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            var top_of_window = $(window).scrollTop();
+            /* If the object is completely visible in the window, fade it it */
+            if (bottom_of_window > bottom_of_object && top_of_window < top_of_object) {
+                $(this).addClass("visible");
+            } else {
+                $(this).removeClass("visible");
+            }
+        });
     });
-  });
 });
