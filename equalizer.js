67b01b5bd573368c296d6a45ec3f704dce4b4142
Jakub Kwiatkowski <jakub1kwiatkowski@gmail.com>
2018-01-22 13:48:59 +0100

    not hiding stripes, renames, fat-stripes

--- equalizer.js
+++ equalizer.js
@@ -1,8 +1,11 @@
 $(document).ready(function () {
+
+
     /* Every time the window is scrolled ... */
+
     $(window).scroll(function () {
         /* Check the location of each desired element */
-        $(".equilizer").each(function (i) {
+        $(".stripe").each(function (i) {
             var bottom_of_object =
                 $(this).offset().top + $(this).outerHeight();
             var top_of_object = $(this).offset().top;
@@ -11,10 +14,8 @@ $(document).ready(function () {
             var bottom_of_window = $(window).scrollTop() + $(window).height();
             var top_of_window = $(window).scrollTop();
             /* If the object is completely visible in the window, fade it it */
-            if (bottom_of_window > bottom_of_object && top_of_window < top_of_object) {
+            if (bottom_of_window > bottom_of_object) {
                 $(this).addClass("visible");
-            } else {
-                $(this).removeClass("visible");
             }
         });
     });
